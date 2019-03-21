7bf4f70ab6f9c92c7302e91eae08f1d56fc4271b
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-03-21 11:16:40 +0300

    add PeopleList

--- src/components/routes/AdminPage.js
+++ src/components/routes/AdminPage.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { moduleName } from "../../ducks/people";
+import PeopleList from "../people/PeopleList";
+import Loader from "../common/Loader";
 
-export default class AdminPage extends Component {
+class AdminPage extends Component {
   render() {
+    const { loading } = this.props;
+
     return (
       <div>
         <h1>Admin Page</h1>
+        {loading ? <Loader /> : null}
+        <PeopleList />
       </div>
     );
   }
 }
+
+export default connect((state) => ({
+  loading: state[moduleName].loading
+}))(AdminPage);
